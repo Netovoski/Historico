3215a245bf3073c982395e8c3fddfae66867e1be
unknown <netovoski@gmail.com>
2020-10-04 12:44:15 -0300

    correçoes

--- website/admin.py
+++ website/admin.py
@@ -6,5 +6,6 @@ from .models import *
 
 #admin.site.register(item)
 @admin.register(Hist_voo2015, Hist_voo2015_2, Hist_voo2015_3, Hist_voo2015_4, Hist_voo2015_5, Hist_voo2015_6, Hist_voo2015_7, Hist_voo2015_8, Hist_voo2015_9, Hist_voo2015_10, Hist_voo2015_11, Hist_voo2015_12, Hist_voo2016, Hist_voo2016_2, Hist_voo2016_3, Hist_voo2017)
+#admin.site.register(Hist_voo2015)
 class ViewAdmin(ImportExportModelAdmin):
     exclude = ('id', )
